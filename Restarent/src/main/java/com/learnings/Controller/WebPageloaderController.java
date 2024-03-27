d4bc81ce16564950c4fb43dc33c1bda5b1caa16a
Vamsikishore Reddy kanchimireddy <vamsireddy1180@gmail.com>
2024-03-27 10:01:48 +0530

    View Configuration via xml to send page  and loading jsp or html

---
+++ Restarent/src/main/java/com/learnings/Controller/WebPageloaderController.java
@@ -0,0 +1,12 @@
+package com.learnings.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebPageloaderController {
+	@RequestMapping("/isOpen")
+	public String isOpen() {
+		return "mywebpage";
+	}
+}
